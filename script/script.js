22beb3077c9b8683e82a53b34a172fd8122a0deb
Connor Trader <77984719+ctrader321@users.noreply.github.com>
2021-02-12 16:50:38 -0600

    final readme touches

--- script/script.js
+++ script/script.js
@@ -1,25 +1,4 @@
-// function validateEmail(email) {
-//     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     return re.test(email);
-//   }
-  
-//   function validate() {
-//     const $result = $("#result");
-//     const email = $("#email").val();
-//     $result.text("");
-  
-//     if (validateEmail(email)) {
-//       $result.text(email + " is valid :)");
-//       $result.css("color", "green");
-//     } else {
-//       $result.text(email + " is not valid :(");
-//       $result.css("color", "red");
-//     }
-//     return false;
-//   }
-  
-//   $("#validate").on("click", validate);
-
+// Used to validate email input
 function ValidateEmail(inputText) {
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(!(inputText.value.match(mailformat))) {
@@ -34,6 +13,7 @@ function ValidateEmail(inputText) {
 
 }
 
+// Used to validate phone number input
 function ValidatePhone(inputNum) {
     var phoneformat = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/;
     if(!(inputNum.value.match(phoneformat))) {
